c8e160fd6d10717e068aa06c82094552e576ccaf
seungdols <seungdols0822@gmail.com>
2022-07-16 14:59:57 +0900

    Add application class

---
+++ src/main/kotlin/com/sample/seungdols/MybatisNeatedApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.seungdols
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MybatisoNeatedApplication
+
+fun main(args: Array<String>) {
+    runApplication<MybatisoNeatedApplication>()
+}
