6891e14f947121fb3862677a8a939fe67d41c757
seungdols <seungdols0822@gmail.com>
2022-07-16 18:42:53 +0900

    Modify Application class

--- src/main/kotlin/com/sample/seungdols/MybatisNeatedApplication.kt
+++ src/main/kotlin/com/sample/seungdols/MybatisNeatedApplication.kt
@@ -1,11 +1,17 @@
 package com.sample.seungdols
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+  scanBasePackages = ["com.sample.seungdols"],
+  exclude = [
+    MybatisAutoConfiguration::class
+  ]
+)
 class MybatisoNeatedApplication
 
 fun main(args: Array<String>) {
-    runApplication<MybatisoNeatedApplication>()
+  runApplication<MybatisoNeatedApplication>()
 }
