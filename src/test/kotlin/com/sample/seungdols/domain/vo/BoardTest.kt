dc20e7dcf646db98615abacb4210fa1ea4b43225
seungdols <seungdols0822@gmail.com>
2022-07-16 18:43:25 +0900

    Add test

---
+++ src/test/kotlin/com/sample/seungdols/domain/vo/BoardTest.kt
@@ -0,0 +1,27 @@
+package com.sample.seungdols.domain.vo
+
+import com.sample.seungdols.domain.BoardMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest
+import org.springframework.test.context.TestConstructor
+
+@MybatisTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+internal class BoardTest(
+  val boardMapper: BoardMapper
+) {
+
+  @Test
+  fun `boardMapper is not null`() {
+    assertThat(boardMapper).isNotNull
+  }
+
+  @Test
+  fun `1번 게시물 정보를 가져온다`() {
+    val boardId = 1
+    val board = boardMapper.getBoardById(boardId)
+
+    println(board.comments)
+  }
+}
